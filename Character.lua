bab08ae24dfc66af745d137ca451521b315fe0e2
ColdIV <yufoolme@gmail.com>
2020-07-11 16:43:40 +0200

    added cd to shooting

--- Character.lua
+++ Character.lua
@@ -2,12 +2,15 @@ local Class = require('libs.Class')
 
 local Character = Class({
     shot = false,
+    shotCD = 0,
+    shotMaxCD = 0.25,
     control = "ai",
     width = 32,
     height = 32,
     speed = 100,
     cLine = {1, 0, 0},
     cFill = {0, 0, 0},
+    cNCFill = {0.5, 0.5, 0.5}, -- "no control"
     projectileSpeed = 150,
     projectileWidth = 16,
     projectileHeight = 16
@@ -36,8 +39,16 @@ function Character:move(direction, width, height, dt)
         end
     end
 
+    if self.shotCD > 0 then
+        self.shotCD = self.shotCD - 1 * dt
+        if self.shotCD < 0 then self.shotCD = 0 end
+    end
+
     if direction[5] == 1 then
-        self:shoot(self.facing, dt)
+        if self.shotCD == 0 then
+            self:shoot(self.facing, dt)
+            self.shotCD = self.shotMaxCD
+        end
     end
 
     local newX, newY = self:getNewPos(self.x, self.y, direction, self.speed, dt)
@@ -53,6 +64,8 @@ function Character:move(direction, width, height, dt)
 end
 
 function Character:moveProjectiles(width, height, dt)
+    width = tonumber(width)
+    height = tonumber(height)
     self.projectiles = self.projectiles or {}
 
     for i = 1, #self.projectiles do
@@ -61,10 +74,10 @@ function Character:moveProjectiles(width, height, dt)
             local newX, newY = self:getNewPos(self.projectiles[i].x, self.projectiles[i].y, self.projectiles[i].direction, self.projectiles[i].speed, dt)
 
             -- check map bounds, @TODO: add some sort of explosion on contact?
-            if newX > width - self.projectiles[i].width then self.projectiles[i].active = false
-            elseif newX < 0 then self.projectiles[i].active = false end
-            if newY > height - self.projectiles[i].height then self.projectiles[i].active = false
-            elseif newY < 0 then self.projectiles[i].active = false end
+            if newX > width then self.projectiles[i].active = false
+            elseif newX < 0 - self.projectiles[i].width then self.projectiles[i].active = false end
+            if newY > height then self.projectiles[i].active = false
+            elseif newY < 0 - self.projectiles[i].height then self.projectiles[i].active = false end
 
             self.projectiles[i].x = newX
             self.projectiles[i].y = newY
@@ -85,7 +98,8 @@ function Character:drawProjectiles()
     for i = 1, #self.projectiles do
         if self.projectiles[i].active == true then
             love.graphics.setColor(self.projectiles[i].color)
-            love.graphics.rectangle("fill", self.projectiles[i].x, self.projectiles[i].y, self.projectiles[i].width, self.projectiles[i].height)
+            love.graphics.circle("fill", self.projectiles[i].x + self.projectileWidth / 2, self.projectiles[i].y  + self.projectileHeight / 2, self.projectileWidth / 2)
+            -- love.graphics.rectangle("fill", self.projectiles[i].x, self.projectiles[i].y, self.projectiles[i].width, self.projectiles[i].height)
         end
     end
 end
